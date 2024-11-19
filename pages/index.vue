187b808ec25a4174368639e0578a84e14d6e7f99
Evan Shimizu <ebshimizu@gmail.com>
2024-11-19 01:21:27 -0800

    changing a word

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@ const filteredReps = computed(() => {
   <div class="md:container mx-auto flex flex-col gap-2">
     <div class="text-4xl text-center">what the rep doin?</div>
     <div class="text-xl text-center">
-      See what your representatives have been voting on at a glance.
+      See what your representatives have been voting on
     </div>
     <UInput
       size="xl"
