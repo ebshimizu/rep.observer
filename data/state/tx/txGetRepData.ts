28ad34b38457dae214a0d09cbeffc01c51587638
Evan Shimizu <ebshimizu@gmail.com>
2025-02-08 21:31:27 -0800

    TX initial data cache (parties need manual correct)

--- data/state/tx/txGetRepData.ts
+++ data/state/tx/txGetRepData.ts
@@ -46,7 +46,7 @@ async function processTxSenate() {
     level: 'state',
     state: 'TX',
     chamber: 'Senate',
-    title: `${toOrdinal(parseInt(sessionMatch?.[1] ?? '1'))} TX State Senate`,
+    title: `${toOrdinal(parseInt(sessionMatch?.[0] ?? '1'))} TX State Senate`,
     start_date: `${session}-01-01`,
     // current sessions have no defined end date
     end_date: undefined,
@@ -103,7 +103,7 @@ async function processTxSenate() {
       first_name: `${names?.[0]}`,
       last_name: `${names?.[1]}`,
       full_name: `${fullName}`,
-      homepage: `${node.querySelector('.member__link')?.getAttribute('href')}`,
+      homepage: `${TxSenateMemberUrl}?d=${district}`,
       term: {
         party: `${party}`,
         state: 'TX',
@@ -118,9 +118,92 @@ async function processTxSenate() {
   }
 
   console.log('finished senate')
+
+  // i don't feel like duplicating work, do you? returns the session number for the house to re-use
+  return parseInt(sessionMatch?.[0] ?? '0')
 }
 
-await processTxSenate()
+interface TxHouseMemberData {
+  active: number
+  id: number
+  image: string
+  link: string
+  member_bill_code: number
+  member_name: string
+}
+
+async function processTxHouse(sessionNumber: number) {
+  const housePage = await fetch(TxHouseUrl)
+  const houseText = await housePage.text()
+  const houseDom = parse(houseText)
+
+  const houseSession: StateSession = {
+    level: 'state',
+    state: 'TX',
+    chamber: 'House',
+    title: `${toOrdinal(sessionNumber)} TX State House of Representatives`,
+    start_date: `${session}-01-01`,
+    // current sessions have no defined end date
+    end_date: undefined,
+  }
+
+  sessions.push(houseSession)
+
+  // the assembly page is not as nice as the senate
+  // but we still know what the row selector is
+  // been a while since i've thought about drupal
+  const members = JSON.parse(
+    houseDom.querySelector('get-members')?.getAttribute(':members') ?? '{ }'
+  ) as TxHouseMemberData[]
+  members.sort((a, b) => a.member_name.localeCompare(b.member_name))
+
+  for (const member of members) {
+    // create an ID as a provisional ID.
+    const nameReversed = member.member_name
+    const names = nameReversed?.split(',').map((n) => n.trim())
+    const fullName = `${names?.[1]} ${names?.[0]}`
+
+    // unfortunately. manual. party data not available on house website as far as i can tell
+    // const party = node.querySelector('.rt-row__party div p')?.textContent[0]
+
+    // we are going to do CA- + first initial + last name and then check conflicts
+    // if there is a conflict, we will add a number
+    const provisionalId = `TX-${names?.[1][0]}${names?.[0]}`
+
+    // separate query
+    const repPage = parse(await (await fetch(member.link)).text())
+    const district = parseInt(
+      repPage.querySelector('main h1')?.text.match(/(\d+)/gi)?.[0] ?? '0'
+    )
+
+    // this will be necessary for the next year of incoming representatives
+    // we should flag duplicates to enable manual checks before fully processing
+    const id = await validateIdUnique(provisionalId, `${fullName}`, memberData)
+
+    // put data in the cache
+    memberData[id] = {
+      id,
+      first_name: `${names?.[1]}`,
+      last_name: `${names?.[0]}`,
+      full_name: `${fullName}`,
+      homepage: member.link,
+      term: {
+        party: 'R', // we will put R and then correct manually since R outnumbers D in TX usually
+        state: 'TX',
+        district: district,
+        sessionIndex: 1,
+      },
+    }
+
+    console.log(
+      `[Assembly] Adding ${id} ${fullName} to cache (${memberData[id].term.party}-${memberData[id].term.district})`
+    )
+  }
+}
+
+const sessionNumber = await processTxSenate()
+await processTxHouse(sessionNumber)
+
 const fileOut = `./cache/${session}/members/members.json`
 fs.writeFileSync(
   fileOut,
