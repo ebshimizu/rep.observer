ae4cefcff112c4782e430bd7a9ed66b9de869cff
Evan Shimizu <ebshimizu@gmail.com>
2025-02-12 21:16:21 -0800

    wip: texas parsing

--- data/state/tx/TxExitCodes.ts
+++ data/state/tx/TxExitCodes.ts
@@ -1,5 +1,9 @@
 // texas is the 28th so that's the prefix
 export enum TxExitCode {
   OK = 0,
-  FTP_FAILURE = 28001
+  GENERAL_FAILURE = 28000,
+  FTP_FAILURE = 28001,
+  SESSION_IDS_MISSING = 28002,
+  JOURNAL_FAILURE = 28003,
+  BILL_SCAN_FAILURE = 28004
 }
